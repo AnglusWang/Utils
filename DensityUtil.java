b5b7dc7da0be34fd491ca4b66f4d395c4286e825
AnglusWang <angluswang@163.com>
2016-11-28 14:35:15 +0800

    [添加] - 添加 DensityUtil 类，可获取屏幕宽度，dip 与 px 单位互换

---
+++ DensityUtil.java
@@ -0,0 +1,32 @@
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+public class DensityUtil {
+
+	/**
+     * 获取屏幕的宽度
+     */
+    public final static int getWindowsWidth(Activity activity) {
+        DisplayMetrics dm = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        return dm.widthPixels;
+    }
+
+	/**
+	 * dip转像素
+	 */
+	public static int dip2px(Context context, float dpValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (dpValue * scale + 0.5f);
+	}
+
+	/**
+	 * 像素转dip
+	 */
+	public static int px2dip(Context context, float pxValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (pxValue / scale + 0.5f);
+	}
+}
